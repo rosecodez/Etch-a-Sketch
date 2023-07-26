41d7c9d6a023517fb556d002b8c7da22522300e2
Rose <rosebeats09@gmail.com>
2023-07-25 21:45:44 -0400

    Delete details

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 // Main container
 
 function createDiv() {
-  // create the main div element
   const mainDiv = document.createElement("div");
-  // add the main div into DOM
   document.body.appendChild(mainDiv);
-  // assign ID to the div element
   mainDiv.setAttribute("id","mainDiv");
 }
 createDiv();
 
 // Child containers
 function createChild() {
-  // create the child div element
-  const child = document.createElement("div");
-  // add the child div under mainDiv
-  mainDiv.appendChild(child);
-  // assign class to the child div
-  child.setAttribute("id", "child");
+  const childDiv = document.createElement("div");
+  mainDiv.appendChild(childDiv);
+  childDiv.setAttribute("id", "childDiv");
 }
 createChild();
+
+// Grandchild containers
+function createGranchild() {
+  const grandchild = document.createElement("div");
+  childDiv.appendChild(grandchild);
+  grandchild.setAttribute("id", "grandchild");
+}
+createGranchild()
+
 
 
 //Loop 16 times
@@ -27,7 +30,7 @@ createChild();
 function createLoop() {
   for (let i = 0; i < 15; i++) {
     createChild();
-
+    
   }
 }
 createLoop();
