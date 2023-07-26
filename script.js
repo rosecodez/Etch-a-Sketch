0d3d5375557a1b8bcc9d161eb390df75494c2fdd
Rose <rosebeats09@gmail.com>
2023-07-25 20:45:54 -0400

    Create loop

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 // Main container
 
-// create the main div element
-const mainDiv = document.createElement("div");
-// add the main div into DOM
-document.body.appendChild(mainDiv);
-// assign ID to the div element
-mainDiv.setAttribute("id","mainDiv");
-
+function createDiv() {
+  // create the main div element
+  const mainDiv = document.createElement("div");
+  // add the main div into DOM
+  document.body.appendChild(mainDiv);
+  // assign ID to the div element
+  mainDiv.setAttribute("id","mainDiv");
+}
+createDiv();
 
 // Child containers
+function createChild() {
+  // create the child div element
+  const child = document.createElement("div");
+  // add the child div under mainDiv
+  mainDiv.appendChild(child);
+  // assign class to the child div
+  child.setAttribute("id", "child");
+}
+createChild();
 
-// create the child div element
-const child = document.createElement("div");
-// add the child div under mainDiv
-mainDiv.appendChild(child);
-// assign class to the child div
-child.setAttribute("class", "child");
+
+//Loop 16 times
+
+function createLoop() {
+  for (let i = 0; i < 15; i++) {
+    createChild();
+
+  }
+}
+createLoop();
